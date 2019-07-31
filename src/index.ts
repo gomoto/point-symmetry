5cd9e84f9ce01ced99c5aac1634aac77fc3a9fd8
Ryan Gomoto <ryan@cyrusbio.com>
2019-07-30 20:23:18 -0700

    findPointPairs

--- src/index.ts
+++ src/index.ts
@@ -19,10 +19,30 @@ function findAllSymmetryLines(points: Point[]): Line[] {
 }
 
 function findCandidateSymmetryLines(points: Point[]): Line[] {
+  const pairs: MultiPoint[] = findPointPairs(points);
+  console.log(JSON.stringify(pairs, null, 2));
   return [];
 }
 function doesLineReflectAllPoints(line: Line, points: Point[]): boolean {
   return true;
+}
+
+// Return list of point pairs, unrepeated.
+function findPointPairs(points: Point[]): MultiPoint[] {
+  // N choose 2
+  const pairs: MultiPoint[] = [];
+  for (let p = 0; p < points.length - 1; p++) {
+    for (let q = p + 1; q < points.length; q++) {
+      const pair = {
+        points: [
+          points[p],
+          points[q],
+        ],
+      };
+      pairs.push(pair);
+    }
+  }
+  return pairs;
 }
 
 interface Point {
@@ -33,4 +53,8 @@ interface Point {
 interface Line {
   a: Point;
   b: Point;
+}
+
+interface MultiPoint {
+  points: Point[];
 }
