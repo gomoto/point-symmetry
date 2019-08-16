e2fe57bdfcf36a7209af3d04ec9920fa7eb92a06
Ryan Gomoto <ryan@cyrusbio.com>
2019-08-16 02:05:46 -0700

    Fix typo

--- src/find-symmetry-lines.ts
+++ src/find-symmetry-lines.ts
@@ -39,7 +39,7 @@ export function findSymmetryLines(points: Point[]): Line[] {
   // For N points, there are at most N lines of symmetry.
   // The number of lines of symmetry is a factor of N or of N - 1 (when one point coincides with the global center).
   // For each factor in decreasing order starting from N, rotate the first line by π/N.
-  // Return the first factor where all points are reflected by the rotated line.
+  // Find the first factor where all points are reflected by the rotated line.
   const N = points.length;
   const factors = [
     ...factorize(N),
